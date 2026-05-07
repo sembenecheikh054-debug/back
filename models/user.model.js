05b2a44149b5567eab407fb2d3aeb3e7a2fc8b1c
Cheikh <sembenecheikh054@gmail.com>
2026-05-07 15:44:55 +0000

    tout le code collé

--- models/user.model.js
+++ models/user.model.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     nom:      { type: String, required: true },
     email:    { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    isActive: {type:Boolean,default:false},
+    ActivationToken:{type:String},
+    
 });
 
 module.exports = mongoose.model("User", userSchema);
