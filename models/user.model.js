13bebbb8164338fe33027fd7a719ae30660c1079
Cheikh <sembenecheikh054@gmail.com>
2026-05-07 15:53:51 +0000

    je suis revenu

--- models/user.model.js
+++ models/user.model.js
@@ -5,8 +5,7 @@ const userSchema = new mongoose.Schema({
     nom:      { type: String, required: true },
     email:    { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isActive: {type:Boolean,default:false},
-    ActivationToken:{type:String},
+  
     
 });
 
