999e0b3bf8209c3d90b4c34687987569817b671c
Cheikh <sembenecheikh054@gmail.com>
2026-05-07 13:30:24 +0000

    fix mongodb atlas connection

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        mongoose.set('strictQuery', false);
-        await mongoose.connect("mongodb://localhost:27017/red");
+        mongoose.set("strictQuery", false);
+
+        await mongoose.connect(process.env.MONGO_URI);
+
         console.log("Mongo connecté ✅");
+
     } catch (error) {
         console.error("Erreur MongoDB :", error.message);
         process.exit(1);
